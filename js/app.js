4442e30287e19d6cdc465bd4f07d3624672b09ed
riffon3000 <riffon@gmail.com>
2018-10-04 17:59:05 -0500

    merge JS file; move script tag links to bottom of html doc; add relative file paths

--- js/app.js
+++ js/app.js
@@ -3,3 +3,14 @@ $(document).ready(function(){
    $('#modal').modal();
    $('#modal').modal('open'); 
 });
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD6nJlQQmvGywIq2nVo1MLLRkV3y1WYi2s",
+    authDomain: "skynetdate.firebaseapp.com",
+    databaseURL: "https://skynetdate.firebaseio.com",
+    projectId: "skynetdate",
+    storageBucket: "skynetdate.appspot.com",
+    messagingSenderId: "888406917389"
+  };
+  firebase.initializeApp(config);
