0fd8d0aafc12cdb9aba9bf6f23d8cc73fc76a6ab
Aurom Thebes <joshehenry@yahoo.com>
2018-10-06 13:59:09 -0500

    JS changes, working on implementing both API's

--- testNapsterApi.js
+++ testNapsterApi.js
@@ -6,18 +6,18 @@ var isPlaying = true;
 
 // function to play or pause audio
 function Jukebox(){ 
-
+  
   this.play_pause = () => {
-      if(isPlaying){
-        player.pause();
-        isPlaying  = !isPlaying;
-        }
-      else{
-        player.play();
-        isPlaying  = !isPlaying;
-      }
+    if(isPlaying){
+      player.pause();
+      isPlaying  = !isPlaying;
+    }
+    else{
+      player.play();
+      isPlaying  = !isPlaying;
+    }
   }
-// play this clicked
+  // play this clicked
   this.playSelected = (event) => {
     handleSound($(event).attr("data-song"));
   }
@@ -39,41 +39,81 @@ var apiKey = "MjYxOTU3NTItZjAzZC00YTJlLWI0NTItY2IzYTkzNmNjZTc0";
 $('#artist').on({
   input:(evt) => {
     if(evt.target.value.length > 3)
-      $.ajax({
-         url: `http://api.napster.com/v2.2/search?apikey=${apiKey}&query=${evt.target.value}&type=artist`
-         ,
-         success: function(response){
-            options_list.empty();
-            for(var i=0;i<5;i++){
-                options_list.append(`<div class="option" data-id=${response.search.data.artists[i].id} onClick="loadTracks(this)">${response.search.data.artists[i].name}</div>`);
-            }
-          }
-        })
-    },
-    focus: (evt) => {
-      $('#search-option-list').css({'display':'flex'});
-    }
-
-})
-
-// function called to load 10 tracks
-function loadTracks(evt){
-  var artist_id;
-  artist_id = (typeof evt === 'string')? evt : evt.getAttribute('data-id');
-  $.ajax({
-      url:`http://api.napster.com/v2.2/artists/${artist_id}/tracks/top?apikey=${apiKey}&limit=10`
+    $.ajax({
+      url: `http://api.napster.com/v2.2/search?apikey=${apiKey}&query=${evt.target.value}&type=artist`
       ,
       success: function(response){
-        var arrSongs = response.tracks;
-          arrSongs.forEach((elem) => {
-          song_list.prepend(
-          `<div class="music-card" data-song=${elem.previewURL} style="background-image:url(http://direct.napster.com/imageserver/v2/albums/${elem.albumId}/images/200x200.jpg);" onClick="jukeBox.playSelected(this)">
-            <h5>${elem.name}</h6>
-            <div class="songImg">
-            </div>
-           </div>`)
-        });
-      handleSound(arrSongs[0].previewURL);
+        options_list.empty();
+        for(var i=0;i<5;i++){
+          options_list.append(`<div class="option" data-id=${response.search.data.artists[i].id} onClick="loadTracks(this)">${response.search.data.artists[i].name}</div>`);
+          
+        }
       }
-    });
-}
+    })
+  },
+  focus: (evt) => {
+    $('#search-option-list').css({'display':'flex'});
+  }
+  
+})
+
+var artists = $("#artist").val().trim();
+
+
+function SeatGeek() {
+  var queryURL = "https://api.seatgeek.com/2/performers?slug="+artists+"&client_id=OTA5NzI3MnwxNTM4NTMyNDM0LjI0"
+  
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(results) {
+    artists = (results.performers[0].url);
+    
+    console.log(results.performers[0].url);
+    console.log(results.performers[0].slug);
+  
+    //var arr = results;
+    
+   
+  //ajax call for SeatGeek API -----------------------------------------------------------------------------------------------------
+  
+    //
+    
+    })}
+    // function called to load 10 tracks
+    function loadTracks(evt){
+      var artist_id;
+      artist_id = (typeof evt === 'string')? evt : evt.getAttribute('data-id');
+      $.ajax({
+        url:`http://api.napster.com/v2.2/artists/${artist_id}/tracks/top?apikey=${apiKey}&limit=10`
+        ,
+        success: function(response){
+          var arrSongs = response.tracks;
+          arrSongs.forEach((elem) => {
+            song_list.prepend(
+              `<div class="music-card" data-song=${elem.previewURL} style="background-image:url(http://direct.napster.com/imageserver/v2/albums/${elem.albumId}/images/200x200.jpg);" onClick="jukeBox.playSelected(this)">
+             <a rel="noopener noreferrer" target="_blank" href=${artists}> <div class="songImg"> Click me for tickets!
+            <h5>${elem.name}</h6> 
+            </div>
+            </a>
+            </div>`)
+          });
+          handleSound(arrSongs[0].previewURL);
+        }
+      });
+    }
+
+    //ajax call for SeatGeek API---------------------------------------------------------------------------------------
+
+    SeatGeek();
+    
+    // Initialize Firebase
+      var config = {
+      apiKey: "AIzaSyD6nJlQQmvGywIq2nVo1MLLRkV3y1WYi2s",
+      authDomain: "skynetdate.firebaseapp.com",
+      databaseURL: "https://skynetdate.firebaseio.com",
+      projectId: "skynetdate",
+      storageBucket: "skynetdate.appspot.com",
+      messagingSenderId: "888406917389"
+      };
+      firebase.initializeApp(config);
